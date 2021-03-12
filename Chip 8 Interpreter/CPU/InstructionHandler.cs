f082f84ad7e27dd12a8fe99418fa8e40286296e9
Sergio <sergiod.roddj@gmail.com>
2021-03-12 14:34:39 -0400

    Add project files.

---
+++ Chip 8 Interpreter/CPU/InstructionHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interpreter.CPU
+{
+    class InstructionHandler
+    {
+    }
+}
---
+++ Chip 8 Interpreter/Core/Chip8.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interpreter.Core
+{
+    static class Chip8
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
