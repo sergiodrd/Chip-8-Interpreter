4e6365a9bf495d6c2a721a241bedcad08a6c5560
Sergio <sergiod.roddj@gmail.com>
2021-03-13 22:23:09 -0400

    Implemented display logic with raylib

--- Chip 8 Interpreter/Chip8.cs
+++ Chip 8 Interpreter/Chip8.cs
@@ -1,15 +1,29 @@
-﻿namespace ChipSharp
+﻿using Raylib_cs;
+
+namespace ChipSharp
 {
     class Chip8
     {
         static void Main()
         {
-            var rl = new RomLoader("IBM Logo.ch8");
-            var fl = new FontLoader("font.txt");
+            var romName = "IBM Logo.ch8";
+            var fontName = "font.txt";
+            var width = 64;
+            var height = 32;
+            var resolutionMultiplier = 15;
+            var bgColor = new Color(177, 82, 65, 255);
+            var fgColor = new Color(246, 192, 82, 255);
+
+            var rl = new RomLoader(romName);
+            var fl = new FontLoader(fontName);
             var memoryHandler = new MemoryHandler(rl, fl);
+            var displayHandler = new DisplayHandler(width, height, resolutionMultiplier, bgColor, fgColor);
 
             memoryHandler.LoadFont(0x50);
             memoryHandler.LoadRom(0x200);
+
+            displayHandler.Start();
+
         }
     }
 }
--- Chip 8 Interpreter/DisplayHandler.cs
+++ Chip 8 Interpreter/DisplayHandler.cs
@@ -1,10 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Raylib_cs;
 
 namespace ChipSharp
 {
     class DisplayHandler
     {
+        private int width;
+        private int height;
+        private int resolutionMultiplier;
+        private Color bgColor;
+        private Color fgColor;
+
+        private bool[,] grid;
+
+        public DisplayHandler(int width, int height, int resolutionMultiplier, Color bgColor, Color fgColor)
+        {
+            this.width = width;
+            this.height = height;
+            this.resolutionMultiplier = resolutionMultiplier;
+            this.bgColor = bgColor;
+            this.fgColor = fgColor;
+
+            grid = new bool[width, height];
+        }
+
+        public void Start()
+        {
+            Raylib.InitWindow(width * resolutionMultiplier, height * resolutionMultiplier, "Chip Sharp");
+        }
+
+        public void Clear()
+        {
+            for(int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++) { grid[x, y] = false; }
+            }
+            Raylib.ClearBackground(bgColor);
+        }
+
+        public void FlipPixel(int a, int b)
+        {
+            grid[a, b] = !grid[a, b];
+            var x = a * resolutionMultiplier;
+            var y = b * resolutionMultiplier;
+            var color = grid[a, b] ? fgColor : bgColor;
+            Raylib.DrawRectangle(x, y, resolutionMultiplier, resolutionMultiplier, color);
+        }
     }
 }
